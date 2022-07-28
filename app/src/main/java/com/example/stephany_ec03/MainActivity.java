c336ce3607f758ef7f98c4bd60b3e3b64e526a5d
Stephany <A20203917@idat.edu.pe>
2022-07-28 12:36:31 -0500

    buenas tardes

---
+++ app/src/main/java/com/example/stephany_ec03/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.stephany_ec03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
